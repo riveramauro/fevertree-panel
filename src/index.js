df561609d9673edbc6b0c4089b19ee811775f171
Mauricio Rivera <maurocol03@gmail.com>
2022-04-20 13:35:23 -0400

    Update to use React 18

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { HashRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
+root.render(
   <HashRouter>
     <App />
-  </HashRouter>,
-  document.getElementById('root')
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
